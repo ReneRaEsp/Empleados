a1af0b13ddd33393392728193abb48e181aa8dee
Rene <rdejramirez@gmail.com>
2021-05-08 20:24:50 -0500

    agregada funcion de agregar usuario a firebase de forma satisfactoria

--- src/app/components/create-empleados/create-empleados.component.ts
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-create-empleados',
@@ -10,7 +12,9 @@ export class CreateEmpleadosComponent implements OnInit {
 
   createEmpleado: FormGroup;
   submitted = false;
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private _empleadosService: EmpleadoService,
+    private router: Router) {
     this.createEmpleado = this.fb.group({
       nombre: ['', Validators.required ],
       apellido: ['', Validators.required],
@@ -35,7 +39,13 @@ export class CreateEmpleadosComponent implements OnInit {
       fechaCreacion: new Date(),
       fechaActualizacion: new Date()      
     }
-    console.log(empleado);
+    console.log(`registrando el empleado:  ${empleado.nombre} ${empleado.apellido} `);
+    this._empleadosService.agregarEmpleado(empleado).then(()=>{
+      console.log(`Empleado: ${empleado.nombre} ${empleado.apellido} registrado con exito`);
+      this.router.navigate(['/list-empleados']);
+    }).catch(error => {
+      console.log(`Error: ${error}`);
+    });
   }
 
 }
